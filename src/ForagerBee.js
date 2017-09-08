37ff561f5428f7d63fcb864867627447b794b105
TiffyB <tiffanyabarth@gmail.com>
2017-09-08 10:12:40 -0700

    finished 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(age, job) {
+  	super(age, job)
+  	this.age = 10
+  	this.canFly = true
+  	this.job = "find pollen"
+  	this.treasureChest = []
+  }
+  forage(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, color, job) {
+  	super(age, color, job)
+  	this.age = 40
+  	this.canFly = false
+  	this.color = "grey"
+  	this.job = "gamble"
+  }
+  forage() {
+  	return "I am too old, let me play cards instead"
+  }
+  gamble(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
+
